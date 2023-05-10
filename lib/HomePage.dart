3b69867c7e8a27a75f9f2084e4009f1af4b1c565
kuzeyfe <50907921+kuzeyfe@users.noreply.github.com>
2023-05-10 20:01:25 +0300

    proje başlatma
    ilk kurulum

---
+++ lib/HomePage.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Deneme Uygulaması'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.blue,
+            padding: EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // İlk butonun işlevselliği buraya yazılabilir
+                  },
+                  child: Text('Anasayfa'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // İkinci butonun işlevselliği buraya yazılabilir
+                  },
+                  child: Text('Buton 2'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Üçüncü butonun işlevselliği buraya yazılabilir
+                  },
+                  child: Text('Buton 3'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Dördüncü butonun işlevselliği buraya yazılabilir
+                  },
+                  child: Text('Buton 4'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Beşinci butonun işlevselliği buraya yazılabilir
+                  },
+                  child: Text('Buton 5'),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Text(
+                'Hoşgeldiniz',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
